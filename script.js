2bf1660f9f8dc56aff08470e1b5c956501364ab6
Kavi0828 <maduryakavi@gmail.com>
2025-12-13 02:03:19 -0800

    Add files via upload
    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToContact() {
+document.getElementById("contact").scrollIntoView();
+}
+
+
+function showMessage() {
+document.getElementById("success").innerText = "Message sent successfully!";
+return false;
+}
